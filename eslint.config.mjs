4bbaabdbce8cf573a58d986260e599a8e1ea2ab1
Munrhalls <antarcticdepths71@gmail.com>
2026-07-04 13:19:54 +0200

    Difficulty: 2 - D, Config (frontend): remove nx-welcome, update index.html, styles, eslint config → DoD:0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,10 +27,20 @@ export default [
                     ],
                     depConstraints: [
                         {
+                            sourceTag: "type:app",
+                            onlyDependOnLibsWithTags: ["type:feature", "type:ui", "type:data-access", "type:util"]
+                        },
+                        {
+                            sourceTag: "type:e2e",
+                            onlyDependOnLibsWithTags: ["type:app"]
+                        },
+                        {
+                            sourceTag: "type:backend",
+                            onlyDependOnLibsWithTags: ["type:util", "type:data-access"]
+                        },
+                        {
                             sourceTag: "*",
-                            onlyDependOnLibsWithTags: [
-                                "*"
-                            ]
+                            onlyDependOnLibsWithTags: ["*"]
                         }
                     ]
                 }
